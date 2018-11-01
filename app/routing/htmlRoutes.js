95ce3ce8958c0e2861b31e357013fc3a59f08951
Eric Keyes <keyes.eric@outlook.comgit config --global user.email keyes.eric@outlook.com>
2018-10-31 23:53:59 -0400

    web update 2

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
 
 
     //route for all other inquiries
-    app.get("*", function(req, res){
+    app.get("/", function(req, res){
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
