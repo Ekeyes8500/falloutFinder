5af6237b5d6b0929424e230054d8bb375650e484
Eric Keyes <keyes.eric@outlook.comgit config --global user.email keyes.eric@outlook.com>
2018-11-01 00:01:53 -0400

    port fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 require("jquery");
 
 // initial port
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
